4dd2637eab99b1623a29b40ba19d752a564d2cf1
Filipe Vieira <filipe.silva@cidadaoon.com>
2024-03-15 17:29:38 -0300

    feat: aula 2

--- classroom/src/http/graphql/resolvers/products.resolver.ts
+++ classroom/src/http/graphql/resolvers/products.resolver.ts
@@ -1,15 +1,15 @@
 import { UseGuards } from '@nestjs/common'
 import { Query, Resolver } from '@nestjs/graphql'
 import { PrismaService } from 'src/database/prisma/prisma.service'
-import { AuthorizationGuard } from './auth/authorization.guard'
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard'
 
 @Resolver()
-export class TestResolver {
-  constructor(private prismaService: PrismaService) { }
+export class ProductsResolver {
+  constructor(private prisma: PrismaService) { }
 
   @Query(() => String)
   @UseGuards(AuthorizationGuard)
-  hello() {
-    return 'Hello, World!'
+  products() {
+    return this.prisma.
   }
 }
--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -3,8 +3,8 @@ import { ConfigModule } from '@nestjs/config'
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo'
 import { GraphQLModule } from '@nestjs/graphql'
 import { resolve } from 'node:path'
-import { TestResolver } from './test.resolver'
 import { DatabaseModule } from 'src/database/database.module'
+import { ProductsResolver } from './graphql/resolvers/products.resolver'
 
 @Module({
   imports: [
@@ -15,6 +15,6 @@ import { DatabaseModule } from 'src/database/database.module'
       autoSchemaFile: resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [ProductsResolver],
 })
 export class HttpModule { }
